dfb73b3f8c73b4af2b28c78d2f5e5f0eab09fe7c
kaleido <lyf63101@gmail.com>
2024-08-31 14:57:15 +0800

    feat: submit

---
+++ test/CCNS.t.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {CCIPLocalSimulatorFork, Register} from "@chainlink/local/src/ccip/CCIPLocalSimulatorFork.sol";
+
+import {CrossChainNameServiceLookup} from "../src/CrossChainNameServiceLookup.sol";
+import {CrossChainNameServiceReceiver} from "../src/CrossChainNameServiceReceiver.sol";
+import {CrossChainNameServiceRegister} from "../src/CrossChainNameServiceRegister.sol";
+
+contract CCNSTest is Test {
+    CCIPLocalSimulatorFork public ccipLocalSimulatorFork;
+    uint256 ethSepoliaFork;
+    uint256 arbSepoliaFork;
+    Register.NetworkDetails ethSepoliaNetworkDetails;
+    Register.NetworkDetails arbSepoliaNetworkDetails;
+
+    address me;
+    CrossChainNameServiceLookup public ethSepoliaCCNSLookup;
+    CrossChainNameServiceLookup public arbSepoliaCCNSLookup;
+    CrossChainNameServiceReceiver public ethSepoliaCCNSReceiver;
+    CrossChainNameServiceRegister public arbSepoliaCCNSRegister;
+
+    function setUp() public {
+        me = makeAddr("0x7fe7A1b34987D0C5848A006F22323731E55Ff2C6");
+
+        ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
+        vm.makePersistent(address(ccipLocalSimulatorFork));
+
+        string memory ETHEREUM_SEPOLIA_RPC_URL = vm.envString(
+            "ETHEREUM_SEPOLIA_RPC_URL"
+        );
+        string memory ARBITRUM_SEPOLIA_RPC_URL = vm.envString(
+            "ARBITRUM_SEPOLIA_RPC_URL"
+        );
+        ethSepoliaFork = vm.createFork(ETHEREUM_SEPOLIA_RPC_URL);
+        arbSepoliaFork = vm.createSelectFork(ARBITRUM_SEPOLIA_RPC_URL);
+
+        arbSepoliaNetworkDetails = ccipLocalSimulatorFork.getNetworkDetails(
+            block.chainid
+        );
+        vm.selectFork(ethSepoliaFork);
+
+        // step 1, deploy lookup and receiver in ehtereum sepolia
+        assertEq(vm.activeFork(), ethSepoliaFork);
+
+        ethSepoliaNetworkDetails = ccipLocalSimulatorFork.getNetworkDetails(
+            block.chainid
+        ); // 目前我们处于Ethereum Sepolia的分叉网络中
+        assertEq(
+            ethSepoliaNetworkDetails.chainSelector,
+            16015286601757825753,
+            "Sanity check: Ethereum Sepolia chain selector should be 16015286601757825753"
+        );
+
+        ethSepoliaCCNSLookup = new CrossChainNameServiceLookup();
+        ethSepoliaCCNSReceiver = new CrossChainNameServiceReceiver(
+            ethSepoliaNetworkDetails.routerAddress,
+            address(ethSepoliaCCNSLookup),
+            arbSepoliaNetworkDetails.chainSelector
+        );
+        ethSepoliaCCNSLookup.setCrossChainNameServiceAddress(
+            address(ethSepoliaCCNSReceiver)
+        );
+
+        // step 2, deploy lookup and register in arbitum sepolia
+        vm.selectFork(arbSepoliaFork);
+        assertEq(vm.activeFork(), arbSepoliaFork);
+        // 目前我们处于Arbitrum Sepolia的分叉网络中
+        assertEq(
+            arbSepoliaNetworkDetails.chainSelector,
+            3478487238524512106,
+            "Sanity check: Arbitrum Sepolia chain selector should be 3478487238524512106"
+        );
+
+        arbSepoliaCCNSLookup = new CrossChainNameServiceLookup();
+        arbSepoliaCCNSRegister = new CrossChainNameServiceRegister(
+            arbSepoliaNetworkDetails.routerAddress,
+            address(arbSepoliaCCNSLookup)
+        );
+        arbSepoliaCCNSLookup.setCrossChainNameServiceAddress(
+            address(arbSepoliaCCNSRegister)
+        );
+    }
+
+    function test_arbSepoliaRegister() public {
+        vm.startPrank(me);
+        arbSepoliaCCNSRegister.register("hello");
+
+        assertEq(arbSepoliaCCNSLookup.lookup("hello"), address(me));
+
+        vm.selectFork(ethSepoliaFork);
+        assertEq(ethSepoliaCCNSLookup.lookup("hello"), address(0));
+        vm.stopPrank();
+    }
+
+    function test_crossChainRegister() public {
+        address(arbSepoliaCCNSRegister).call{value: 1 ether}("");
+        arbSepoliaCCNSRegister.enableChain(
+            ethSepoliaNetworkDetails.chainSelector,
+            address(ethSepoliaCCNSReceiver),
+            2000000
+        );
+
+        assertEq(address(arbSepoliaCCNSRegister).balance, 10 ** 18);
+
+        vm.startPrank(me);
+        arbSepoliaCCNSRegister.register("hello");
+        vm.stopPrank();
+        assertEq(arbSepoliaCCNSLookup.lookup("hello"), address(me));
+
+        ccipLocalSimulatorFork.switchChainAndRouteMessage(ethSepoliaFork); // 这行代码将更换CHAINLINK CCIP DONs, 不要遗漏
+        assertEq(vm.activeFork(), ethSepoliaFork);
+        assertEq(ethSepoliaCCNSLookup.lookup("hello"), address(me));
+    }
+}
